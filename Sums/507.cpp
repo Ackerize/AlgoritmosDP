e0cfc79255ac41eeb2892183cd37e14760b35fbb
DavidVallecios <37277365+DavidVallecios@users.noreply.github.com>
2019-10-28 15:03:08 -0600

    Update 507.cpp

--- Sums/507.cpp
+++ Sums/507.cpp
@@ -1,29 +1,39 @@
-#include <stdio.h>
+#include <iostream>
+#include <climits>
+#include <cstdio>
 
-int main() {
-	int testcase, cases = 0;
-	int n, x;
-	scanf("%d", &testcase);
-    while(testcase--) {
-        scanf("%d", &n);
-        int tmp = 1, st = 0xffff, ed, sum = 0, ans = 0;
-        for (int i = 2; i <= n; i++) {
-            scanf("%d", &x);
-            sum += x;
-            if (sum < 0) sum = 0, tmp = i;
-            if (sum >= ans) {
-            	
-                if (sum > ans || (sum == ans && (i - tmp > ed - st))) {
-                    st = tmp;
-                    ed = i;
-                }
-                ans = sum;
+
+using namespace std;
+
+int main()
+{
+    int test, N, curSum, curSI, curEI, MS, maxSI, maxEI, cs;
+    cs = 1;
+    int A[20005];
+    scanf("%d", &test);
+    while(test--){
+        scanf("%d", &N);
+        N--;
+        for(int i = 1; i <= N; i++)
+            scanf("%d", &A[i]);
+
+        curSum = 0;
+        curSI = 1;
+        MS = INT_MIN;
+        for(curEI = 1; curEI <= N; curEI++){
+            curSum += A[curEI];
+            if(curSum < 0){
+                curSI = curEI + 1;
+                curSum = 0;
+            }
+            if(curSum > MS || (curSum == MS && (maxEI-maxSI < curEI- curSI))){
+                MS = curSum;
+                maxSI = curSI;
+                maxEI = curEI;
             }
         }
-        if (ans > 0)
-            printf("The nicest part of route %d is between stops %d and %d\n", ++cases, st, ed);
-        else
-            printf("Route %d has no nice parts\n", ++cases);
+        if(MS > 0)printf("The nicest part of route %d is between stops %d and %d\n", cs++, maxSI, maxEI+1);
+        else printf("Route %d has no nice parts\n", cs++);
     }
     return 0;
 }
